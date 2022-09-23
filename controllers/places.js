905fd72dca6d387cceb23cab2f63399d0b500c3b
Rub <melikyan740@gmail.com>
2022-09-23 12:18:39 -0700

    Adding routers table to READEME

--- controllers/places.js
+++ controllers/places.js
@@ -2,9 +2,15 @@ const router = require("express").Router()
 
 router.get("/", function(req, res){
 
-res.send("Get /places")
+res.send("Places Index Page")
 
 
 })
 
+router.post("/", function(req, res){
+
+    res.send("Creat New Places")
+    
+    
+    })
 module.exports = router
