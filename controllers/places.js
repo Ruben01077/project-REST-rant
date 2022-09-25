2a1adf88aa7f6a9ff6f03fe960f3b12fe37efc62
Rub <melikyan740@gmail.com>
2022-09-25 13:48:56 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,38 +1,32 @@
 const router = require("express").Router()
+const places = require("../models/places")
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+
 router.get('/', (req, res) => {
-
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'images/seattle-cafe.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'images/phoenix-cafe.jpg'
-  },]
-
   res.render('places/index', {places})
-
-})
-
-router.get('/', (req, res) => {
-  res.render('places/index')
 })
 
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body) // push to the array
+  res.redirect('/places') // after you have added a new place with the POST route, we can redirect to the index route to see our newly added place!
 })
+
 
 
 
