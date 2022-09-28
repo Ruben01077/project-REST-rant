2321c40c47318df1d676ea3532b4ca6828228b3e
Rub <melikyan740@gmail.com>
2022-09-27 17:33:41 -0700

    Add the edit form

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -2,6 +2,7 @@ const React = require('react')
 const Def = require('../default.jsx')
 
 function edit_form(data) {
+ 
   return (
     <Def>
       <main>
@@ -16,27 +17,27 @@ function edit_form(data) {
 
           <h1>Edit Place</h1>
 
-          <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+          <form method="POST" action={`/places/${data.id}?_method=PUT`}>
             <div className="row">
             <div className="form-group">
             <label htmlFor="name">Place Name</label>
-            <input className="form-control" id="name" name="name" required />
+            <input className="form-control" id="name" name="name" value={data.place.name} required />
           </div>
           <div className="form-group">
             <label htmlFor="pic">Place Picture</label>
-            <input className="form-control" id="pic" name="pic" />
+            <input className="form-control" id="pic" name="pic" value={data.place.pic} />
           </div>
           <div className="form-group">
             <label htmlFor="city">City</label>
-            <input className="form-control" id="city" name="city" required />
+            <input className="form-control" id="city" name="city" value={data.place.city} required  />
           </div>
           <div className="form-group">
             <label htmlFor="state">State</label>
-            <input className="form-control" id="state" name="state" required />
+            <input className="form-control" id="state" name="state" value={data.place.state} required />
           </div>
           <div className="form-group">
             <label htmlFor="cuisines">Cuisines</label>
-            <input className="form-control" id="cuisines" name="cuisines" required />
+            <input className="form-control" id="cuisines" name="cuisines" value={data.place.cuisines} required />
           </div>
             </div>
 
